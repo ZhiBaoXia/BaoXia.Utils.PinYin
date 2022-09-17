a1b1689a2f1d453bb197722d36d1e51bbb21c4c8
Zxc-PC\Zxc <zhu.xiaochun@qq.com>
2022-09-17 21:36:58 +0800

    完善了拼音功能相关的字符串扩展类。

--- BaoXia.Utils.PinYin.Test/ChinesePinYinInfoTest.cs
+++ BaoXia.Utils.PinYin.Test/ChinesePinYinInfoTest.cs
@@ -8,6 +8,43 @@ namespace BaoXia.Utils.PinYin.Test
                 [TestMethod]
                 public void GetPinYinInfoWithChineseCharacterTest()
                 {
+                        ////////////////////////////////////////////////
+                        // 1/2，测试ASCII表中的所有字符：
+                        ////////////////////////////////////////////////
+                        for (var asciiCharIndex = 0;
+                                asciiCharIndex < 256;
+                                asciiCharIndex++)
+                        {
+                                var asciiChar = (char)asciiCharIndex;
+                                var asciiCharacter = new string(asciiChar, 1);
+                                var asciiCharPinYinInfo = BaoXia.Utils.PinYin.ChinesePinYinInfo
+                                        .GetPinYinInfoWithChineseCharacter(asciiCharacter);
+
+                                // 0~31及127(共33个)是控制字符或通信专用字符
+                                if (asciiChar >= 32
+                                        && asciiChar != 127)
+                                {
+                                        // !!!
+                                        Assert.IsNotNull(asciiCharPinYinInfo);
+                                        // !!!
+
+                                        // !!!
+                                        Assert.IsTrue(asciiCharPinYinInfo.Value.PinYin == asciiCharacter);
+                                        // !!!
+                                }
+                                else
+                                {
+                                        // !!!
+                                        Assert.IsNull(asciiCharPinYinInfo);
+                                        // !!! 
+                                }
+                        }
+
+
+                        ////////////////////////////////////////////////
+                        // 2/2，测试字符表中的所有汉字：
+                        ////////////////////////////////////////////////
+
                         var chinesePinYinOriginalInfes
                                 = ChinesePinYinOriginalInfes.Infes;
                         foreach (var chinesePinYinOriginalInfo in chinesePinYinOriginalInfes)
@@ -37,7 +74,6 @@ namespace BaoXia.Utils.PinYin.Test
                                 // !!!
                         }
                 }
-
 
                 [TestMethod]
                 public void GetPinYinInfesWithChineseCharacterTest()
@@ -83,5 +119,70 @@ namespace BaoXia.Utils.PinYin.Test
                         }
                 }
 
+
+                class TestItem
+                {
+                        public string ChineseCharacter { get; set; }
+                        public string PinYin { get; set; }
+                        public string PinYinWithCommaSeparator { get; set; }
+                        public string PinYinWithFirstChar { get; set; }
+
+                        public TestItem(
+                                string chineseCharacter,
+                                string pinYin,
+                                string pinYinWithCommaSeparator,
+                                string pinYinWithFirstChar)
+                        {
+                                this.ChineseCharacter = chineseCharacter;
+                                this.PinYin = pinYin;
+                                this.PinYinWithCommaSeparator = pinYinWithCommaSeparator;
+                                this.PinYinWithFirstChar = pinYinWithFirstChar;
+                        }
+                }
+
+                [TestMethod]
+                public void GetPinYinOfStringTest()
+                {
+                        var testItems = new List<TestItem>()
+                        {
+                                new ("汉字",
+                                        "HanZi",
+                                        "Han,Zi",
+                                        "HZ"),
+                                new ("HanZi汉字",
+                                        "HanZiHanZi",
+                                        "H,a,n,Z,i,Han,Zi",
+                                        "HanZiHZ"),
+                                new ("汉字HanZi",
+                                        "HanZiHanZi",
+                                        "Han,Zi,H,a,n,Z,i",
+                                        "HZHanZi"),
+                                new ("HanZi汉字HanZi",
+                                        "HanZiHanZiHanZi",
+                                        "H,a,n,Z,i,Han,Zi,H,a,n,Z,i",
+                                        "HanZiHZHanZi"),
+                                new ("汉字HanZi汉字",
+                                        "HanZiHanZiHanZi",
+                                        "Han,Zi,H,a,n,Z,i,Han,Zi",
+                                        "HZHanZiHZ"),
+                        };
+                        foreach (var testItem in testItems)
+                        {
+                                var pinYin = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter);
+                                // !!!
+                                Assert.IsTrue(testItem.PinYin.Equals(pinYin, StringComparison.OrdinalIgnoreCase));
+                                // !!!
+
+                                var pinYinWithCommaSeparator = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter, false, ",");
+                                // !!!
+                                Assert.IsTrue(testItem.PinYinWithCommaSeparator.Equals(pinYinWithCommaSeparator, StringComparison.OrdinalIgnoreCase));
+                                // !!!
+
+                                var pinYinWithFirstChar = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter, true);
+                                // !!!
+                                Assert.IsTrue(testItem.PinYinWithFirstChar.Equals(pinYinWithFirstChar, StringComparison.OrdinalIgnoreCase));
+                                // !!!
+                        }
+                }
         }
 }
--- BaoXia.Utils.PinYin/Extension/StringExtension.cs
+++ BaoXia.Utils.PinYin/Extension/StringExtension.cs
@@ -14,120 +14,60 @@ namespace BaoXia.Utils.PinYin.Extension
                 /// </summary>
                 /// <param name="str">当前字符串。</param>
                 /// <returns>当前字符串对应的拼音字符串列表。</returns>
-                public static List<List<string>>? ToPinYinsList(this string str)
+                public static List<List<string>>? ToPinYinsList(
+                        this string str)
                 {
                         if (str.Length < 1)
                         {
                                 return null;
                         }
 
+
+                        var pinYinInfoLists = ChinesePinYinInfo.GetPinYinInfesOfString(str);
+                        if (pinYinInfoLists == null
+                                || pinYinInfoLists.Count < 1)
+                        {
+                                return null;
+                        }
+
                         // 记录每个字符的全拼
                         List<List<string>> pinYinsList = new();
-                        /*
-                        for (int charIndex = 0;
-                            charIndex < str.Length;
-                            charIndex++)
+                        foreach (var pinYinInfoList in pinYinInfoLists)
                         {
-                                var character = str[charIndex];
-                                var charPinYins = new List<string>();
-                                if (ChineseChar.IsValidChar(character))
+                                if (pinYinInfoList == null)
                                 {
-                                        var chineseChar = new ChineseChar(character);
-                                        if (chineseChar.Pinyins?.Count > 0)
+                                        continue;
+                                }
+
+                                var pinYins = new List<string>();
+                                foreach (var pinYinInfo in pinYinInfoList)
+                                {
+                                        if (pinYinInfo.PinYin?.Length > 0)
                                         {
-                                                foreach (var chineseCharPinYin in chineseChar.Pinyins)
-                                                {
-                                                        if (!string.IsNullOrWhiteSpace(chineseCharPinYin))
-                                                        {
-                                                                var charPinYin = chineseCharPinYin;
-                                                                while (charPinYin.Length > 0)
-                                                                {
-                                                                        var lastPinYinChar = charPinYin[^1];
-                                                                        // ⚠ 去除声调标记 ⚠
-                                                                        if (lastPinYinChar >= '0'
-                                                                            && lastPinYinChar <= '9')
-                                                                        {
-                                                                                charPinYin = charPinYin[0..^1];
-                                                                        }
-                                                                        else
-                                                                        {
-                                                                                break;
-                                                                        }
-                                                                }
-                                                                // !!!
-                                                                if (!charPinYins.Contains(charPinYin))
-                                                                {
-                                                                        charPinYins.Add(charPinYin);
-                                                                }
-                                                                // !!!
-                                                        }
-                                                }
+                                                pinYins.Add(pinYinInfo.PinYin);
                                         }
                                 }
-                                else
-                                {
-                                        // !!!
-                                        charPinYins.Add(character.ToString());
-                                        // !!!
-                                }
-                                // !!!
-                                pinYinsList.Add(charPinYins);
-                                // !!!
+                                pinYinsList.Add(pinYins);
+
                         }
-                        */
                         return pinYinsList;
                 }
-
 
                 /// <summary>
                 /// 将当前字符串，转为对应的拼音字符串。
                 /// </summary>
                 /// <param name="str">当前字符串。</param>
+                /// <param name="isNeedUpperCase">是否返回大写字母的拼音字符串。</param>
                 /// <returns>当前字符串对应的拼音字符串。</returns>
-                public static string? ToPinYin(this string str)
+                public static string? ToPinYin(
+                        this string str,
+                        bool isNeedUpperCase = true)
                 {
-                        string? pinYin = null;
-                        var pinYinsList = str.ToPinYinsList();
-                        if (pinYinsList?.Count > 0)
+                        var pinYin = ChinesePinYinInfo.GetPinYinOfString(str);
+                        if (isNeedUpperCase
+                                && pinYin != null)
                         {
-                                foreach (var pinYins in pinYinsList)
-                                {
-                                        if (pinYins.Count > 0)
-                                        {
-                                                var pinYinChars = pinYins[0];
-                                                var finalPinYinChars = new char[pinYinChars.Length];
-                                                for (var pinYinCharIndex = 0;
-                                                    pinYinCharIndex < pinYinChars.Length;
-                                                    pinYinCharIndex++)
-                                                {
-                                                        var pinYinChar = pinYinChars[pinYinCharIndex];
-                                                        if (pinYinCharIndex == 0)
-                                                        {
-                                                                if (pinYinChar >= 'a'
-                                                                    && pinYinChar <= 'z')
-                                                                {
-                                                                        pinYinChar = (char)(pinYinChar
-                                                                            + ('A' - 'a'));
-                                                                }
-                                                        }
-                                                        else
-                                                        {
-                                                                if (pinYinChar >= 'A'
-                                                                    && pinYinChar <= 'Z')
-                                                                {
-                                                                        pinYinChar = (char)(pinYinChar
-                                                                            + ('a' - 'A'));
-                                                                }
-                                                        }
-                                                        //
-                                                        finalPinYinChars[pinYinCharIndex] = pinYinChar;
-                                                        //
-                                                }
-                                                // !!!
-                                                pinYin += new string(finalPinYinChars);
-                                                // !!!
-                                        }
-                                }
+                                pinYin = pinYin.ToUpper();
                         }
                         return pinYin;
                 }
@@ -136,51 +76,21 @@ namespace BaoXia.Utils.PinYin.Extension
                 /// 将当前字符串，转为对应的首字母拼音字符串。
                 /// </summary>
                 /// <param name="str">当前字符串。</param>
-                /// <param name="isToUpperCase">是否返回大写字母的拼音字符串。</param>
+                /// <param name="isNeedUpperCase">是否返回大写字母的拼音字符串。</param>
                 /// <returns>当前字符串对应的首字母拼音字符串。</returns>
                 public static string? ToPinYinWithFirstCharacter(
-                    this string str,
-                    bool isToUpperCase = true)
+                        this string str,
+                        bool isNeedUpperCase = true)
                 {
-                        string? pinYin = null;
-                        var pinYinsList = str.ToPinYinsList();
-                        if (pinYinsList?.Count > 0)
+                        var pinYin = ChinesePinYinInfo.GetPinYinOfString(
+                                str,
+                                true);
+                        if (isNeedUpperCase
+                                && pinYin != null)
                         {
-                                foreach (var pinYins in pinYinsList)
-                                {
-                                        if (pinYins.Count > 0)
-                                        {
-                                                var pinYinChars = pinYins[0];
-                                                if (pinYinChars.Length > 0)
-                                                {
-                                                        var pinYinChar = pinYinChars[0];
-                                                        if (isToUpperCase)
-                                                        {
-                                                                if (pinYinChar >= 'a'
-                                                                    && pinYinChar <= 'z')
-                                                                {
-                                                                        pinYinChar = (char)(pinYinChar
-                                                                            + ('A' - 'a'));
-                                                                }
-                                                        }
-                                                        else
-                                                        {
-                                                                if (pinYinChar >= 'A'
-                                                                    && pinYinChar <= 'Z')
-                                                                {
-                                                                        pinYinChar = (char)(pinYinChar
-                                                                            + ('a' - 'A'));
-                                                                }
-                                                        }
-                                                        // !!!
-                                                        pinYin += pinYinChar;
-                                                        // !!!
-                                                }
-                                        }
-                                }
+                                pinYin = pinYin.ToUpper();
                         }
                         return pinYin;
                 }
-
         }
 }
