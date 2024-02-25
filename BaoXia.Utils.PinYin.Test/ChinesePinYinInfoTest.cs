e5cb8104ff7353f099cd6aa0d2008ab4033c943a
zhu xiaochun <zhu.xiaochun@qq.com>
2024-02-25 10:35:34 +0800

    发布了： 8.0.0 版本。

--- BaoXia.Utils.PinYin.Test/ChinesePinYinInfoTest.cs
+++ BaoXia.Utils.PinYin.Test/ChinesePinYinInfoTest.cs
@@ -1,188 +1,187 @@
 using BaoXia.Utils.PinYin.ChinesePinYinInfesCodeCreator;
 
-namespace BaoXia.Utils.PinYin.Test
+namespace BaoXia.Utils.PinYin.Test;
+
+[TestClass]
+public class ChinesePinYinOriginalInfoTest
 {
-	[TestClass]
-	public class ChinesePinYinOriginalInfoTest
+	[TestMethod]
+	public void GetPinYinInfoWithChineseCharacterTest()
 	{
-		[TestMethod]
-		public void GetPinYinInfoWithChineseCharacterTest()
+		////////////////////////////////////////////////
+		// 1/2，测试ASCII表中的所有字符：
+		////////////////////////////////////////////////
+		for (var asciiCharIndex = 0;
+			asciiCharIndex < 256;
+			asciiCharIndex++)
 		{
-			////////////////////////////////////////////////
-			// 1/2，测试ASCII表中的所有字符：
-			////////////////////////////////////////////////
-			for (var asciiCharIndex = 0;
-				asciiCharIndex < 256;
-				asciiCharIndex++)
+			var asciiChar = (char)asciiCharIndex;
+			var asciiCharacter = new string(asciiChar, 1);
+			var asciiCharPinYinInfo = BaoXia.Utils.PinYin.ChinesePinYinInfo
+				.GetPinYinInfoWithChineseCharacter(asciiCharacter);
+
+			// 0~31及127(共33个)是控制字符或通信专用字符
+			if (asciiChar >= 32
+				&& asciiChar != 127)
 			{
-				var asciiChar = (char)asciiCharIndex;
-				var asciiCharacter = new string(asciiChar, 1);
-				var asciiCharPinYinInfo = BaoXia.Utils.PinYin.ChinesePinYinInfo
-					.GetPinYinInfoWithChineseCharacter(asciiCharacter);
-
-				// 0~31及127(共33个)是控制字符或通信专用字符
-				if (asciiChar >= 32
-					&& asciiChar != 127)
-				{
-					// !!!
-					Assert.IsNotNull(asciiCharPinYinInfo);
-					// !!!
-
-					// !!!
-					Assert.IsTrue(asciiCharPinYinInfo.Value.PinYin == asciiCharacter);
-					// !!!
-				}
-				else
-				{
-					// !!!
-					Assert.IsNull(asciiCharPinYinInfo);
-					// !!! 
-				}
-			}
-
-
-			////////////////////////////////////////////////
-			// 2/2，测试字符表中的所有汉字：
-			////////////////////////////////////////////////
-
-			var chinesePinYinOriginalInfes
-				= ChinesePinYinOriginalInfes.Infes;
-			foreach (var chinesePinYinOriginalInfo in chinesePinYinOriginalInfes)
-			{
-				var testPinYinInfo = BaoXia.Utils.PinYin.ChinesePinYinInfo
-					.GetPinYinInfoWithChineseCharacter(
-					chinesePinYinOriginalInfo.ChineseCharacter);
 				// !!!
-				Assert.IsNotNull(testPinYinInfo);
+				Assert.IsNotNull(asciiCharPinYinInfo);
 				// !!!
 
-
-				var originalPinYins = chinesePinYinOriginalInfo.PinYin.Split(
-					',',
-					StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-
 				// !!!
-				Assert.IsTrue(testPinYinInfo.Value.PinYin == originalPinYins[0]);
-				// !!!
-
-
-				var originalPinYinWithSounds = chinesePinYinOriginalInfo.PinYinWithSound.Split(
-					',',
-					StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-				// !!!
-				Assert.IsTrue(testPinYinInfo.Value.PinYinWithSound == originalPinYinWithSounds[0]);
+				Assert.IsTrue(asciiCharPinYinInfo.Value.PinYin == asciiCharacter);
 				// !!!
 			}
-		}
-
-		[TestMethod]
-		public void GetPinYinInfesWithChineseCharacterTest()
-		{
-			var chinesePinYinOriginalInfes
-				= ChinesePinYinOriginalInfes.Infes;
-			foreach (var chinesePinYinOriginalInfo in chinesePinYinOriginalInfes)
+			else
 			{
-				var testPinYinInfes = BaoXia.Utils.PinYin.ChinesePinYinInfo
-					.GetPinYinInfesWithChineseCharacter(
-					chinesePinYinOriginalInfo.ChineseCharacter);
 				// !!!
-				Assert.IsNotNull(testPinYinInfes);
-				// !!!
-
-
-				var originalPinYins = chinesePinYinOriginalInfo.PinYin.Split(
-					',',
-					StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-				// !!!
-				Assert.IsTrue(testPinYinInfes.Count == originalPinYins.Length);
-				// !!!
-				foreach (var testPinYinInfo in testPinYinInfes)
-				{
-					// !!!
-					Assert.IsTrue(originalPinYins.Contains(testPinYinInfo.PinYin) == true);
-					// !!!
-				}
-
-
-				var originalPinYinWithSounds = chinesePinYinOriginalInfo.PinYinWithSound.Split(
-					',',
-					StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-				// !!!
-				Assert.IsTrue(testPinYinInfes.Count == originalPinYinWithSounds.Length);
-				// !!!
-				foreach (var testPinYinInfo in testPinYinInfes)
-				{
-					// !!!
-					Assert.IsTrue(originalPinYinWithSounds.Contains(testPinYinInfo.PinYinWithSound) == true);
-					// !!!
-				}
+				Assert.IsNull(asciiCharPinYinInfo);
+				// !!! 
 			}
 		}
 
 
-		class TestItem
-		{
-			public string ChineseCharacter { get; set; }
-			public string PinYin { get; set; }
-			public string PinYinWithCommaSeparator { get; set; }
-			public string PinYinWithFirstChar { get; set; }
+		////////////////////////////////////////////////
+		// 2/2，测试字符表中的所有汉字：
+		////////////////////////////////////////////////
 
-			public TestItem(
-				string chineseCharacter,
-				string pinYin,
-				string pinYinWithCommaSeparator,
-				string pinYinWithFirstChar)
+		var chinesePinYinOriginalInfes
+			= ChinesePinYinOriginalInfes.Infes;
+		foreach (var chinesePinYinOriginalInfo in chinesePinYinOriginalInfes)
+		{
+			var testPinYinInfo = BaoXia.Utils.PinYin.ChinesePinYinInfo
+				.GetPinYinInfoWithChineseCharacter(
+				chinesePinYinOriginalInfo.ChineseCharacter);
+			// !!!
+			Assert.IsNotNull(testPinYinInfo);
+			// !!!
+
+
+			var originalPinYins = chinesePinYinOriginalInfo.PinYin.Split(
+				',',
+				StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+			// !!!
+			Assert.IsTrue(testPinYinInfo.Value.PinYin == originalPinYins[0]);
+			// !!!
+
+
+			var originalPinYinWithSounds = chinesePinYinOriginalInfo.PinYinWithSound.Split(
+				',',
+				StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+			// !!!
+			Assert.IsTrue(testPinYinInfo.Value.PinYinWithSound == originalPinYinWithSounds[0]);
+			// !!!
+		}
+	}
+
+	[TestMethod]
+	public void GetPinYinInfesWithChineseCharacterTest()
+	{
+		var chinesePinYinOriginalInfes
+			= ChinesePinYinOriginalInfes.Infes;
+		foreach (var chinesePinYinOriginalInfo in chinesePinYinOriginalInfes)
+		{
+			var testPinYinInfes = BaoXia.Utils.PinYin.ChinesePinYinInfo
+				.GetPinYinInfesWithChineseCharacter(
+				chinesePinYinOriginalInfo.ChineseCharacter);
+			// !!!
+			Assert.IsNotNull(testPinYinInfes);
+			// !!!
+
+
+			var originalPinYins = chinesePinYinOriginalInfo.PinYin.Split(
+				',',
+				StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+			// !!!
+			Assert.IsTrue(testPinYinInfes.Count == originalPinYins.Length);
+			// !!!
+			foreach (var testPinYinInfo in testPinYinInfes)
 			{
-				this.ChineseCharacter = chineseCharacter;
-				this.PinYin = pinYin;
-				this.PinYinWithCommaSeparator = pinYinWithCommaSeparator;
-				this.PinYinWithFirstChar = pinYinWithFirstChar;
+				// !!!
+				Assert.IsTrue(originalPinYins.Contains(testPinYinInfo.PinYin) == true);
+				// !!!
+			}
+
+
+			var originalPinYinWithSounds = chinesePinYinOriginalInfo.PinYinWithSound.Split(
+				',',
+				StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+			// !!!
+			Assert.IsTrue(testPinYinInfes.Count == originalPinYinWithSounds.Length);
+			// !!!
+			foreach (var testPinYinInfo in testPinYinInfes)
+			{
+				// !!!
+				Assert.IsTrue(originalPinYinWithSounds.Contains(testPinYinInfo.PinYinWithSound) == true);
+				// !!!
 			}
 		}
+	}
 
-		[TestMethod]
-		public void GetPinYinOfStringTest()
+
+	class TestItem
+	{
+		public string ChineseCharacter { get; set; }
+		public string PinYin { get; set; }
+		public string PinYinWithCommaSeparator { get; set; }
+		public string PinYinWithFirstChar { get; set; }
+
+		public TestItem(
+			string chineseCharacter,
+			string pinYin,
+			string pinYinWithCommaSeparator,
+			string pinYinWithFirstChar)
 		{
-			var testItems = new List<TestItem>()
-			{
-				new ("汉字",
-					"HanZi",
-					"Han,Zi",
-					"HZ"),
-				new ("HanZi汉字",
-					"HanZiHanZi",
-					"H,a,n,Z,i,Han,Zi",
-					"HanZiHZ"),
-				new ("汉字HanZi",
-					"HanZiHanZi",
-					"Han,Zi,H,a,n,Z,i",
-					"HZHanZi"),
-				new ("HanZi汉字HanZi",
-					"HanZiHanZiHanZi",
-					"H,a,n,Z,i,Han,Zi,H,a,n,Z,i",
-					"HanZiHZHanZi"),
-				new ("汉字HanZi汉字",
-					"HanZiHanZiHanZi",
-					"Han,Zi,H,a,n,Z,i,Han,Zi",
-					"HZHanZiHZ"),
-			};
-			foreach (var testItem in testItems)
-			{
-				var pinYin = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter);
-				// !!!
-				Assert.IsTrue(testItem.PinYin.Equals(pinYin, StringComparison.OrdinalIgnoreCase));
-				// !!!
+			this.ChineseCharacter = chineseCharacter;
+			this.PinYin = pinYin;
+			this.PinYinWithCommaSeparator = pinYinWithCommaSeparator;
+			this.PinYinWithFirstChar = pinYinWithFirstChar;
+		}
+	}
 
-				var pinYinWithCommaSeparator = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter, false, ",");
-				// !!!
-				Assert.IsTrue(testItem.PinYinWithCommaSeparator.Equals(pinYinWithCommaSeparator, StringComparison.OrdinalIgnoreCase));
-				// !!!
+	[TestMethod]
+	public void GetPinYinOfStringTest()
+	{
+		var testItems = new List<TestItem>()
+		{
+			new ("汉字",
+				"HanZi",
+				"Han,Zi",
+				"HZ"),
+			new ("HanZi汉字",
+				"HanZiHanZi",
+				"H,a,n,Z,i,Han,Zi",
+				"HanZiHZ"),
+			new ("汉字HanZi",
+				"HanZiHanZi",
+				"Han,Zi,H,a,n,Z,i",
+				"HZHanZi"),
+			new ("HanZi汉字HanZi",
+				"HanZiHanZiHanZi",
+				"H,a,n,Z,i,Han,Zi,H,a,n,Z,i",
+				"HanZiHZHanZi"),
+			new ("汉字HanZi汉字",
+				"HanZiHanZiHanZi",
+				"Han,Zi,H,a,n,Z,i,Han,Zi",
+				"HZHanZiHZ"),
+		};
+		foreach (var testItem in testItems)
+		{
+			var pinYin = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter);
+			// !!!
+			Assert.IsTrue(testItem.PinYin.Equals(pinYin, StringComparison.OrdinalIgnoreCase));
+			// !!!
 
-				var pinYinWithFirstChar = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter, true);
-				// !!!
-				Assert.IsTrue(testItem.PinYinWithFirstChar.Equals(pinYinWithFirstChar, StringComparison.OrdinalIgnoreCase));
-				// !!!
-			}
+			var pinYinWithCommaSeparator = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter, false, ",");
+			// !!!
+			Assert.IsTrue(testItem.PinYinWithCommaSeparator.Equals(pinYinWithCommaSeparator, StringComparison.OrdinalIgnoreCase));
+			// !!!
+
+			var pinYinWithFirstChar = ChinesePinYinInfo.GetPinYinOfString(testItem.ChineseCharacter, true);
+			// !!!
+			Assert.IsTrue(testItem.PinYinWithFirstChar.Equals(pinYinWithFirstChar, StringComparison.OrdinalIgnoreCase));
+			// !!!
 		}
 	}
 }
