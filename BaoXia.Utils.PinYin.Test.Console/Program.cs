2e910f5c245c4b23c3122e28ff849b6715f89f51
ZhuXiaoChun <zhu.xiaochun@qq.com>
2024-07-04 18:55:24 +0800

    增加了：“BaoXia.Utils.PinYin.Test.Console”测试项目。

---
+++ BaoXia.Utils.PinYin.Test.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using BaoXia.Utils.PinYin;
+
+var testText = "“你好, 世界!”的拼音是：";
+Console.WriteLine(testText);
+var testTextPinYin
+	= ChinesePinYinInfo.GetPinYinOfString(testText);
+Console.WriteLine(testTextPinYin);
+var testTextPinYinFirstChar
+	= ChinesePinYinInfo.GetPinYinOfString(testText, true);
+Console.WriteLine(testTextPinYinFirstChar);
+
+Console.ReadLine();
+
